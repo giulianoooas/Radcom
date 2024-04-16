40a540d0f3b01f4e2327349ecb0dbd5dbfd2466d
giulianoooas <florentingiulianodumitru@gmail.com>
2024-04-16 21:46:46 +0300

    fix pages and animation from home

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -2,44 +2,80 @@
 import "./footer.component.css";
 
 export const Footer = () => {
-  return  <footer>
-	<div class="container">
-		<div class="row">
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <svg
+            className="svgcolor-light"
+            preserveAspectRatio="none"
+            viewBox="0 0 100 102"
+            height="100"
+            width="100%"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0 L50 100 L100 0 Z"></path>
+          </svg>
 
-      <svg class="svgcolor-light" preserveAspectRatio="none" viewBox="0 0 100 102" height="100" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 0 L50 100 L100 0 Z"></path>
-      </svg>
-
-      <div class="col-md-4 col-sm-6">
-        <h2><strong>Radcom</strong></h2>
-          <div class="wow fadeInUp" data-wow-delay="0.3s">
-             <p>About Us</p>
+          <div className="col-md-4 col-sm-6">
+            <h2>
+              <strong>Radcom</strong>
+            </h2>
+            <div className="wow fadeInUp" data-wow-delay="0.3s">
+              <p>About Us</p>
               <p>Services</p>
               <p>News</p>
-             <p> Investors
-             </p>
-             <p class="copyright-text">Copyright &copy; 2024 Your Company </p>
+              <p> Investors</p>
+              <p className="copyright-text">
+                Copyright &copy; 2024 Your Company{" "}
+              </p>
+            </div>
           </div>
+
+          <div className="col-md-1 col-sm-1"></div>
+
+          <div className="col-md-4 col-sm-5">
+            <h2>
+              <strong>Contact us</strong>
+            </h2>
+            <p className="wow fadeInUp" data-wow-delay="0.6s">
+              120-240 aliquam augue libero, Convallis in vulputate 10220 San
+              Francisco, CA, USA
+            </p>
+            <ul className="social-icon">
+              <li>
+                <a
+                  href="/"
+                  className="fa fa-facebook wow bounceIn"
+                  data-wow-delay="0.9s"
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="fa fa-twitter wow bounceIn"
+                  data-wow-delay="1.2s"
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="fa fa-behance wow bounceIn"
+                  data-wow-delay="1.4s"
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  className="fa fa-dribbble wow bounceIn"
+                  data-wow-delay="1.6s"
+                ></a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-
-      <div class="col-md-1 col-sm-1"></div>
-
-      <div class="col-md-4 col-sm-5">
-        <h2><strong>Contact us</strong></h2>
-        <p class="wow fadeInUp" data-wow-delay="0.6s">
-          120-240 aliquam augue libero,
-          Convallis in vulputate 10220 
-          San Francisco, CA, USA
-        </p>
-        <ul class="social-icon">
-          <li><a href="/" class="fa fa-facebook wow bounceIn" data-wow-delay="0.9s"></a></li>
-          <li><a href="/" class="fa fa-twitter wow bounceIn" data-wow-delay="1.2s"></a></li>
-          <li><a href="/" class="fa fa-behance wow bounceIn" data-wow-delay="1.4s"></a></li>
-          <li><a href="/" class="fa fa-dribbble wow bounceIn" data-wow-delay="1.6s"></a></li>
-        </ul>
-      </div>
-
-		</div>
-	</div>
-</footer>
+    </footer>
+  );
 };
--- src/components/landing/landing.component.js
+++ src/components/landing/landing.component.js
@@ -1,19 +1,54 @@
+import { useState, useEffect } from "react";
 import "./landing.component.css";
+import slide1 from "./../../assets/slide-1.jpg";
+import slide2 from "./../../assets/slide-2.jpg";
 
 export const Landing = () => {
-    return <section id="home">
-	<div className="container">
-		<div className="row">
+  const slideImages = [slide1, slide2];
+  const [currentSlideIdx, setCurrentSlideIdx] = useState(0);
+  let lastIdx = 0;
 
-			<div className="col-md-offset-2 col-md-8 col-sm-12">
-				<div className="home-thumb">
-				 <h1 className="wow fadeInUp background" data-wow-delay="0.4s">Hello, we are <span className="main-color">Radcom</span></h1>
-          			<h3 className="wow fadeInUp background" data-wow-delay="0.6s">Lorem ipsum <strong>dolor lorem</strong> ipsum <strong>dolor lorem</strong> ipsum</h3>
-        			<a href="/" data-toggle="modal" data-target="#modal1" className="btn btn-lg btn-success smoothScroll wow fadeInUp" data-wow-delay="1.0s">About Us</a>
-				</div>
-			</div>
+  useEffect(() => {
+    const timer = setInterval(() => {
+      lastIdx = (lastIdx + 1) % slideImages.length;
+      setCurrentSlideIdx(lastIdx);
+    }, 2000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-		</div>
-	</div>		
-</section>
+  return (
+    <section id="home">
+      <img
+        src={slideImages[currentSlideIdx]}
+        alt="slide image"
+        className="slide-image"
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-offset-2 col-md-8 col-sm-12">
+            <div className="home-thumb">
+              <h1 className="wow fadeInUp background" data-wow-delay="0.4s">
+                Hello, we are <span className="main-color">Radcom</span>
+              </h1>
+              <h3 className="wow fadeInUp background" data-wow-delay="0.6s">
+                Lorem ipsum <strong>dolor lorem</strong> ipsum{" "}
+                <strong>dolor lorem</strong> ipsum
+              </h3>
+              <a
+                href="/"
+                data-toggle="modal"
+                data-target="#modal1"
+                className="btn btn-lg btn-success smoothScroll wow fadeInUp"
+                data-wow-delay="1.0s"
+              >
+                About Us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
