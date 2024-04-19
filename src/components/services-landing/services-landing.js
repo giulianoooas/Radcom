05a1b5d4aa6461815c6e8ba1ec0852e60ac9c2e3
giulianoooas <florentingiulianodumitru@gmail.com>
2024-04-19 21:43:24 +0300

    fix services

--- src/components/services-landing/services-landing.js
+++ src/components/services-landing/services-landing.js
@@ -1,126 +1,79 @@
 /* eslint-disable jsx-a11y/alt-text */
+import { useEffect, useRef } from "react";
 import "./services-landing.css";
 
+const defaultService = {
+  imgSrc: "https://dummyimage.com/600x400/ededed/#1c3f9.png",
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+  illo. Ipsam molestias laboriosam facere suscipit, nihil architect o
+  earum illo fugit nisi eos omnis ipsa ratione impedit atque quibusdam
+  libero fugiat. Lorem ipsum dolor sit amet consectetur adipisicing
+  elit. Libero, illo. Ipsam molestias laboriosam facere suscipit,
+  nihil architect o earum illo fugit nisi eos omnis ipsa ratione
+  impedit atque quibusdam libero fugiat. Lorem ipsum dolor sit amet
+  consectetur adipisicing elit. Libero, illo. Ipsam molestias
+  laboriosam facere suscipit, nihil architect o earum illo fugit nisi
+  eos omnis ipsa ratione impedit atque quibusdam libero fugiat.`,
+  buttonText: "More about This Service",
+};
+
+const introductionText = defaultService.text;
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
 export const ServicesLanding = () => {
+  const services = Array.from({ length: 5 }, () => defaultService);
+
   return (
     <section className="services">
       <nav className="navbar"></nav>
       <div className="landing-section">
         <div className="introduction">
           <div className="title main-color">Our Radcom Services</div>
-          <div className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-            illo. Ipsam molestias laboriosam facere suscipit, nihil architect o
-            earum illo fugit nisi eos omnis ipsa ratione impedit atque quibusdam
-            libero fugiat.   Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-            illo. Ipsam molestias laboriosam facere suscipit, nihil architect o
-            earum illo fugit nisi eos omnis ipsa ratione impedit atque quibusdam
-            libero fugiat.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-            illo. Ipsam molestias laboriosam facere suscipit, nihil architect o
-            earum illo fugit nisi eos omnis ipsa ratione impedit atque quibusdam
-            libero fugiat. 
-          </div>
+          <div className="text">{introductionText}</div>
         </div>
       </div>
-      <div className="main">
-        <div className="row">
-          <svg
-            preserveAspectRatio="none"
-            viewBox="0 0 100 102"
-            height="100"
-            width="100%"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            className="svgcolor-light"
-          >
-            <path d="M0 0 L50 100 L100 0 Z"></path>
-          </svg>
-        </div>
-        <div className="containerz reveal">
-          <img src="https://dummyimage.com/600x400/ededed/#1c3f9.png" />
-          <div className="info">
-            <div className="service-name main-color">Service Name</div>
-            <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-              exercitationem repellat illo! Veniam fugit ex voluptatum officia
-              dolorem velit eaque in fuga voluptas ipsa, est asperiores illum
-              minus aspernatur doloremque? Lorem ipsum dolor sit a met
-              consectetur adipisicing elit. Laboriosam quos saepe doloremque
-              temporibus eius ea, animi maxime cum voluptatum sed illum totam
-              perspiciatis, soluta exercitationem! Sit fuga quod iusto sint.
-            </div>
-            <div>
-              <button className="btn">More about This Service</button>
+      {services.map((service, idx) => (
+        <div className={idx % 2 ? "secondary" : "main"} key={idx}>
+          <div className="row">
+            <svg
+              preserveAspectRatio="none"
+              viewBox="0 0 100 102"
+              height="100"
+              width="100%"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              className="svgcolor-light"
+            >
+              <path d="M0 0 L50 100 L100 0 Z"></path>
+            </svg>
+          </div>
+          <div className="containerz reveal">
+            <img src={service.imgSrc} />
+            <div className="info">
+              <div className="service-name main-color">Service Name</div>
+              <div className="description">{service.text}</div>
+              <div>
+                <button className="btn">{service.buttonText}</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="secondary">
-        <div className="row">
-          <svg
-            preserveAspectRatio="none"
-            viewBox="0 0 100 102"
-            height="100"
-            width="100%"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            className="svgcolor-light"
-          >
-            <path d="M0 0 L50 100 L100 0 Z"></path>
-          </svg>
-        </div>
-        <div className="containerz reveal">
-          <img src="https://dummyimage.com/600x400/ededed/#1c3f9.png" />
-          <div className="info">
-            <div className="service-name main-color">Service Name</div>
-            <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-              exercitationem repellat illo! Veniam fugit ex voluptatum officia
-              dolorem velit eaque in fuga voluptas ipsa, est asperiores illum
-              minus aspernatur doloremque? Lorem ipsum dolor sit a met
-              consectetur adipisicing elit. Laboriosam quos saepe doloremque
-              temporibus eius ea, animi maxime cum voluptatum sed illum totam
-              perspiciatis, soluta exercitationem! Sit fuga quod iusto sint.
-            </div>
-            <div>
-              <button className="btn">More about This Service</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="main">
-        <div className="row">
-          <svg
-            preserveAspectRatio="none"
-            viewBox="0 0 100 102"
-            height="100"
-            width="100%"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            className="svgcolor-light"
-          >
-            <path d="M0 0 L50 100 L100 0 Z"></path>
-          </svg>
-        </div>
-        <div className="containerz reveal">
-          <img src="https://dummyimage.com/600x400/ededed/#1c3f9.png" />
-          <div className="info">
-            <div className="service-name main-color">Service Name</div>
-            <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-              exercitationem repellat illo! Veniam fugit ex voluptatum officia
-              dolorem velit eaque in fuga voluptas ipsa, est asperiores illum
-              minus aspernatur doloremque? Lorem ipsum dolor sit a met
-              consectetur adipisicing elit. Laboriosam quos saepe doloremque
-              temporibus eius ea, animi maxime cum voluptatum sed illum totam
-              perspiciatis, soluta exercitationem! Sit fuga quod iusto sint.
-            </div>
-            <div>
-              <button className="btn">More about This Service</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
     </section>
   );
 };
