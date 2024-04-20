18e0b0711625899e74714a36eea2d08ece957e00
giulianoooas <florentingiulianodumitru@gmail.com>
2024-04-20 23:49:27 +0300

    fix article page rendering

--- src/components/home-contact/home-contact.component.js
+++ src/components/home-contact/home-contact.component.js
@@ -4,6 +4,36 @@ import image2 from "./../../assets/headway-5QgIuuBxKwM-unsplash.jpg";
 import image3 from "./../../assets/young-kyung-kim-7zQumtlvvfM-unsplash (2).jpg";
 
 export const HomeContact = () => {
+  const articles = [
+    {
+      imgSrc: image1,
+      headerText: "This is some title",
+      contentText: ` Curabitur convallis ac quam vitae laoreet. Nulla mauris
+    ante, euismod sed lacus sit amet, congue bibendum eros.
+    Etiam mattis lobortis porta. Vestibulum ultrices iaculis
+    enim imperdiet egestas.`,
+      buttonText: "Read more",
+    },
+    {
+      imgSrc: image2,
+      headerText: "This is some title",
+      contentText: ` Curabitur convallis ac quam vitae laoreet. Nulla mauris
+    ante, euismod sed lacus sit amet, congue bibendum eros.
+    Etiam mattis lobortis porta. Vestibulum ultrices iaculis
+    enim imperdiet egestas.`,
+      buttonText: "Read more",
+    },
+    {
+      imgSrc: image3,
+      headerText: "This is some title",
+      contentText: ` Curabitur convallis ac quam vitae laoreet. Nulla mauris
+    ante, euismod sed lacus sit amet, congue bibendum eros.
+    Etiam mattis lobortis porta. Vestibulum ultrices iaculis
+    enim imperdiet egestas.`,
+      buttonText: "Read more",
+    },
+  ];
+
   return (
     <section id="contact">
       <div className="container reveal">
@@ -20,99 +50,34 @@ export const HomeContact = () => {
           </div>
 
           <section className="articles">
-            <article>
-              <div className="article-wrapper">
-                <figure>
-                  <img src={image1} alt="" />
-                </figure>
-                <div className="article-body">
-                  <h2>This is some title</h2>
-                  <p>
-                    Curabitur convallis ac quam vitae laoreet. Nulla mauris
-                    ante, euismod sed lacus sit amet, congue bibendum eros.
-                    Etiam mattis lobortis porta. Vestibulum ultrices iaculis
-                    enim imperdiet egestas.
-                  </p>
-                  <a href="/" className="read-more">
-                    Read more{" "}
-                    <span className="sr-only">about this is some title</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="icon"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                      />
-                    </svg>
-                  </a>
+            {articles.map((article, idx) => (
+              <article key={idx}>
+                <div className="article-wrapper">
+                  <figure>
+                    <img src={article.imgSrc} alt="" />
+                  </figure>
+                  <div className="article-body">
+                    <h2>{article.headerText}</h2>
+                    <p>{article.contentText}</p>
+                    <a href="/" className="read-more">
+                      {article.buttonText}
+                      <span className="sr-only">about this is some title</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </article>
-            <article>
-              <div className="article-wrapper">
-                <figure>
-                  <img src={image2} alt="" />
-                </figure>
-                <div className="article-body">
-                  <h2>This is some title</h2>
-                  <p>
-                    Curabitur convallis ac quam vitae laoreet. Nulla mauris
-                    ante, euismod sed lacus sit amet, congue bibendum eros.
-                    Etiam mattis lobortis porta. Vestibulum ultrices iaculis
-                    enim imperdiet egestas.
-                  </p>
-                  <a href="/" className="read-more">
-                    Read more{" "}
-                    <span className="sr-only">about this is some title</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="icon"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                      />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </article>
-            <article>
-              <div className="article-wrapper">
-                <figure>
-                  <img src={image3} alt="" />
-                </figure>
-                <div className="article-body">
-                  <h2>This is some title</h2>
-                  <p>
-                    Curabitur convallis ac quam vitae laoreet. Nulla mauris
-                    ante, euismod sed lacus sit amet, congue bibendum eros.
-                    Etiam mattis lobortis porta. Vestibulum ultrices iaculis
-                    enim imperdiet egestas.
-                  </p>
-                  <a href="/" className="read-more">
-                    Read more{" "}
-                    <span className="sr-only">about this is some title</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="icon"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                      />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </article>
+              </article>
+            ))}
           </section>
         </div>
       </div>
