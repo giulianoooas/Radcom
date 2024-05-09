6dd5e70d92070076c6e68829edf82534da86b529
giulianoooas <florentingiulianodumitru@gmail.com>
2024-05-10 00:20:28 +0300

    fix landing page

--- src/components/home-landing/home-landing.component.js
+++ src/components/home-landing/home-landing.component.js
@@ -34,27 +34,23 @@ export const HomeLanding = () => {
       <img src={slideImages[1]} alt="slide 2" className={imagesClasses[1]} />
 
       <div className="container landing-container">
-        <div className="row">
-          <div className="col-md-offset-2 col-md-8 col-sm-12">
-            <div className="home-thumb">
-              <h1 className="wow fadeInUp background" data-wow-delay="0.4s">
-                Hello, we are <span className="main-color">Radcom</span>
-              </h1>
-              <h3 className="wow fadeInUp background" data-wow-delay="0.6s">
-                Lorem ipsum <strong>dolor lorem</strong> ipsum{" "}
-                <strong>dolor lorem</strong> ipsum
-              </h3>
-              <Link
-                to={"/home"}
-                data-toggle="modal"
-                data-target="#modal1"
-                className="btn btn-lg btn-success smoothScroll wow fadeInUp"
-                data-wow-delay="1.0s"
-              >
-                About Us
-              </Link>
-            </div>
-          </div>
+        <div className="home-thumb landing-interior-container">
+          <h1 className="wow fadeInUp background" data-wow-delay="0.4s">
+            Hello, we are <span className="main-color">Radcom</span>
+          </h1>
+          <h3 className="wow fadeInUp background" data-wow-delay="0.6s">
+            Lorem ipsum <strong>dolor lorem</strong> ipsum{" "}
+            <strong>dolor lorem</strong> ipsum
+          </h3>
+          <Link
+            to={"/home"}
+            data-toggle="modal"
+            data-target="#modal1"
+            className="btn btn-lg btn-success smoothScroll wow fadeInUp"
+            data-wow-delay="1.0s"
+          >
+            About Us
+          </Link>
         </div>
       </div>
     </section>
