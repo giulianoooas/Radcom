666c14a5c3e944577b79c68328dcdda340f4cbf0
giulianoooas <florentingiulianodumitru@gmail.com>
2024-06-11 23:14:22 +0300

    fix home about section

--- src/components/home-about/home-about.component.js
+++ src/components/home-about/home-about.component.js
@@ -23,44 +23,47 @@ export const HomeAbout = () => {
               </div>
               <div className="wow fadeInUp" data-wow-delay="0.6s">
                 <h2 className="wow fadeInUp" data-wow-delay="0.4s">
-                  Top 3 biggest  <span className="main-color"><b>technology companies</b></span> in Romania
+                  Top 3 biggest{" "}
+                  <span className="main-color">
+                    <b>technology companies</b>
+                  </span>{" "}
+                  in Romania
                 </h2>
                 <p>
-               <span className="main-color">Radcom</span>  has developed and deployed end
-to end solutions, both hardware and
-software, for fleet management and
-passenger information for more than 15
-years.
+                  <span className="main-color">Radcom</span> has developed and
+                  deployed end to end solutions, both hardware and software, for
+                  fleet management and passenger information for more than 15
+                  years.
                 </p>
                 <h2 className="wow fadeInUp" data-wow-delay="0.4s">
-                <span className="main-color"><b>Radcom</b></span> takes care of
-everything
+                  <span className="main-color">
+                    <b>Radcom</b>
+                  </span>{" "}
+                  takes care of everything
                 </h2>
                 <p>
-                The solution package for our clients is full
-stack - software and hardware - so you
-don't have to get 2 providers to manage
-your ............
+                  The solution package for our clients is full stack - software
+                  and hardware - so you don't have to get 2 providers to manage
+                  your ............
                 </p>
                 <h2 className="wow fadeInUp" data-wow-delay="0.4s">
                   For example..
                 </h2>
                 <p>
-                Aici as vrea sa dam un exemplu de client
-important cu care Radcom a lucrat si sa
-explicam succint cum l-a ajutat.
+                  Aici as vrea sa dam un exemplu de client important cu care
+                  Radcom a lucrat si sa explicam succint cum l-a ajutat.
                 </p>
               </div>
-            </div> 
+            </div>
           </div>
-         
-        </div><div className="col-md-6 col-sm-12 image-container">
-            <img
-              src={aboutImage}
-              className="img-responsive wow fadeInUp"
-              alt="About"
-            ></img>
-          </div>
+        </div>
+        <div className="col-md-6 col-sm-12 image-container">
+          <img
+            src={aboutImage}
+            className="home-about-img-responsive wow fadeInUp"
+            alt="About"
+          ></img>
+        </div>
       </div>
     </section>
   );
