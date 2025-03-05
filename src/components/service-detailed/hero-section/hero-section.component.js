82a15925202b36387864984f8faa1bd1a1617c50
giulianoooas <florentingiulianodumitru@gmail.com>
2025-03-05 17:20:14 +0200

    fix last updates

--- src/components/service-detailed/hero-section/hero-section.component.js
+++ src/components/service-detailed/hero-section/hero-section.component.js
@@ -5,14 +5,20 @@ export const HeroSection = (props) => {
   const { name, description, imgSrc } = props.product;
 
   return (
-    <section className="hero-section" style={style}>
-      <div className="hero-text">
-        <h1>{name}</h1>
-        <p>{description}</p>
+    <div className="product-det-hero-container">
+      <section className="hero-section" style={style}>
+        <div className="hero-text">
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+        <div className="hero-image">
+          <Image fileName={imgSrc} alt="Produs" height="50" />
+        </div>
+      </section>
+      <div className="product-det-svg-container">
+        <div className="product-det-border1"></div>
+        <div className="product-det-border2"></div>
       </div>
-      <div className="hero-image">
-        <Image fileName={imgSrc} alt="Produs" height="50" />
-      </div>
-    </section>
+    </div>
   );
 };
